93e55b7fc1a3ce0c1fde156af31f0ee41efcd912
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-02 11:53:27 +0200

    added another api

--- laravel/app/Http/Controllers/UserProjectController.php
+++ laravel/app/Http/Controllers/UserProjectController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\UserProject;
 
-class UserProjectController extends Controller
-{
-    //
+class UserProjectController extends Controller{
+    function get_AllProjects(){
+
+        $userId = auth()->id();
+        $projects = UserProject::with(['user', 'project'])
+        ->where('user_id', $userId) 
+        ->get();
+
+        
+        return response()->json([
+            "projects"=> $projects
+        ]);
+    }
+
 }
