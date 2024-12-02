85860576ff1e1aca73b9d337a5e28a722afaa4ac
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-02 11:55:25 +0200

    added another model..

--- laravel/app/Models/UserProject.php
+++ laravel/app/Models/UserProject.php
@@ -4,7 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserProject extends Model
-{
-    //
+class UserProject extends Model{
+    protected $fillable = [
+        'project_id',
+        'user_id',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
